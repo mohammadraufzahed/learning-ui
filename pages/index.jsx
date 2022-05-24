f2b98a4f9deb4cebc95621bf2ba6f3b58efb03a0
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-05-24 11:09:44 -0700

    Removed the junk className

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="min-h-[100vh] w-full bg-gray-100 font-iranYekan px-9 py-4">
+    <div className="min-h-[100vh] w-full font-iranYekan">
       <Head>
         <title>سامانه خرید و فروش</title>
       </Head>
