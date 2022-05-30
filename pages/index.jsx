37592b75b331d21f335b441ea6d527d6ae077cc9
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-05-29 22:40:58 -0700

    Added the box to index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,32 @@
 import Head from "next/head";
+import { FaMoneyBillAlt } from "react-icons/fa";
 import CategoriesProduct from "../components/CategoriesProduct";
 import Features from "../components/Features";
 import Header from "../components/Header";
 import ResourcesBox from "../components/ResourcesBox";
 import SpecialSellsBox from "../components/SpecialSellsBox";
 import TopStoresBox from "../components/TopStoresBox";
+
+const fakeData = {
+  badges: [
+    {
+      name: "گارانتی بازگشت پول",
+      icon: FaMoneyBillAlt,
+    },
+    {
+      name: "گارانتی بازگشت پول",
+      icon: FaMoneyBillAlt,
+    },
+    {
+      name: "گارانتی بازگشت پول",
+      icon: FaMoneyBillAlt,
+    },
+    {
+      name: "گارانتی بازگشت پول",
+      icon: FaMoneyBillAlt,
+    },
+  ],
+};
 
 export default function Home() {
   return (
@@ -26,6 +48,17 @@ export default function Home() {
       </div>
       <CategoriesProduct />
       <ResourcesBox />
+      <div className="transition-all duration-300 grid grid-cols-1 gap-y-5 mb-10 w-full px-1 py-2 sm:grid-cols-2 md:grid-cols-3 md:px-9 md:py-4 lg:grid-cols-4 lg:gap-x-10">
+        {fakeData.badges.map((item, key) => (
+          <div
+            key={key}
+            className="flex w-[50vw] py-5 mx-auto flex-col justify-center items-center gap-2 shadow-xl shadow-purple-400 sm:w-max sm:px-16 sm:py-5 lg:px-10 lg:py-7"
+          >
+            <item.icon className="text-purple-400 text-6xl" />
+            <span className="font-bold text-lg">{item.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
