9c291efa960ea3c0d66fe067a0c5410471743f55
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-06-01 17:07:41 +0430

    Make the AnnouncementBox background gradiant

--- components/AnnouncementBox/index.jsx
+++ components/AnnouncementBox/index.jsx
@@ -2,8 +2,10 @@ import classes from "./style/index.module.scss";
 
 const AnnouncementBox = ({ title, button }) => {
   return (
-    <div className="bg-gray-200 w-full px-1 py-2 md:px-9 md:py-4 text-center flex flex-col gap-16 h-[50vh] items-center justify-center ">
-      <span className="font-extrabold text-4xl drop-shadow-2xl">{title}</span>
+    <div className="bg-gradient-to-r from-purple-200 to-purple-400 w-full px-1 py-2 md:px-9 md:py-4 text-center flex flex-col gap-16 h-[50vh] items-center justify-center ">
+      <span className="font-extrabold text-4xl drop-shadow-2xl text-white">
+        {title}
+      </span>
       <button className="w-[60%] bg-purple-500 transition-colors duration-300 font-bold text-2xl text-white py-4 hover:bg-purple-700">
         {button}
       </button>
