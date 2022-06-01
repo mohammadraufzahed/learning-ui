30507099ce9fba9dc34ba771ff20b498183e3fa1
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-06-01 15:45:27 +0430

    ALigned the items to center

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -5,7 +5,7 @@ import Hero from "../Hero";
 
 const Header = () => {
   return (
-    <div className="w-full h-max bg-gray-100 px-1 py-2 md:px-9 md:py-4 rounded-b-4xl">
+    <div className="w-full h-max bg-gray-100 px-1 py-2 md:px-9 md:py-4 rounded-b-4xl flex flex-col justify-center items-center">
       <Navbar />
       <Menu />
       <Hero />
